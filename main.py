3c2643ca2ffe1681f3ed7249f56a0e0df0930313
Jordan Juan <126527939+thekidjordann@users.noreply.github.com>
2023-03-09 03:44:23 +0000

    Complete E4

--- main.py
+++ main.py
@@ -2,43 +2,12 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
-    str = 'Python Programming'
-    sub1 = str[0:7]
-    sub2 = str[7:]
-    print (sub1)
-    print (sub2)
-    print (sub1 + sub2)
+  celcius = int(input('Enter the Celcius Degree: '))
+  fahrenheit = (9.0 / 5.0) * celcius + 32
+  print(f'Fahrenheit is {fahrenheit:.2f}')
 
-    coursename = 'Primary Programming'
+  pass
 
-    first = coursename[:7]
-    second = coursename[8:]
-    final = first +' ' + second
-
-    print (first)
-    print (second)
-    print (final)
-
-    coursename = 'Python Programming'
-
-    letters = 'a b c d e f g'
-    letters = letters.split()
-    print (letters)
-    print (letters[0])
-    print (letters[1])
-    print (letters[2])
-
-    cname = 'C++ and Python and Java'
-    cname.split('and')
-
-    coursename = 'Python Programming'
-    coursename = coursename.split()
-    tab = '\n'
-    cname = coursename[1] + tab + coursename[0]
-    print (cname)
-
-    pass
-
-
+  
 if __name__ == '__main__':
     main()
